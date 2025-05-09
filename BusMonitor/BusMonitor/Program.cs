fc122c6ffbefac9ca3e45e03ab9f9788d024aecd
Salah Tamer <ma9060139@gmail.com>
2025-05-09 19:39:21 +0300

    feat: added CORS

--- BusMonitor/BusMonitor/Program.cs
+++ BusMonitor/BusMonitor/Program.cs
@@ -17,6 +17,17 @@ namespace BusMonitor
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
@@ -106,6 +117,7 @@ namespace BusMonitor
             }
 
             app.UseHttpsRedirection();
+            app.UseCors();
             app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
